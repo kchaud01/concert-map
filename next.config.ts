93371f7d3720c31ebc86554edc5ea2748d8610b1
Koushik Chaudhuri <koushikchaudhuri@Koushiks-MacBook-Air.local>
2026-04-25 20:36:30 -0400

    add vercel adapter for next 16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  adapterPath: require.resolve('@vercel/next/adapter'),
 };
 
 export default nextConfig;
